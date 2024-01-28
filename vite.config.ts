76249dd9a1af43fa14135be26806f095dadfa8cb
yuzhenyou <yuzhenyou@outlook.com>
2024-01-28 17:55:56 +0800

    Vite5中vite-plugin-eslint插件没有描述，暂时注释

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import eslint from 'vite-plugin-eslint'
+// import eslint from 'vite-plugin-eslint'  //暂时注释了，vite5中有问题，待作者升级依赖解决
+// https://github.com/gxmari007/vite-plugin-eslint/issues/86
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    eslint()
+    // eslint()
   ],
   resolve: {
     alias: {
