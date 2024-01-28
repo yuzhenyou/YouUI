47cdaa0312e268bccc866c2b5c8fc1cb95d63553
yuzhenyou <yuzhenyou@outlook.com>
2024-01-28 17:36:59 +0800

    版本修改

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,14 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2021": true,
-		"node": true
-	},
-	"overrides": [
-	],
-	"extends": ["plugin:vue/vue3-essential", "@vue/eslint-config-typescript"],
-	"parserOptions": {
-		"ecmaVersion": "latest",
-		"sourceType": "module"
-	},
-	"rules": {
-	},
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
 }
