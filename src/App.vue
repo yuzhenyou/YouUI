dd142ecacd71712e21616b13a03be15082f12622
yuzhenyou <yuzhenyou@outlook.com>
2024-02-05 20:23:47 +0800

    Button组件更新

--- src/App.vue
+++ src/App.vue
@@ -11,31 +11,33 @@ onMounted(() => {
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-  </header>
-
   <main>
-    <Button ref="buttonRef">Test Button</Button>
-    <Button plain >Plain Button</Button>
-    <Button round>Round Button</Button>
-    <Button circle>Circle</Button>
-    <Button disabled>Disabled Button</Button><br/><br/>
+    <h1>Button 按钮</h1>
+    <hr />
+    <Button>Default</Button>
     <Button type="primary">Primary</Button>
     <Button type="success">Success</Button>
     <Button type="info">Info</Button>
     <Button type="warning">Warning</Button>
-    <Button type="danger">Danger</Button><br/><br/>
+    <Button type="danger">Danger</Button><br /><br />
+    <Button plain>Plain</Button>
     <Button type="primary" plain>Primary</Button>
     <Button type="success" plain>Success</Button>
     <Button type="info" plain>Info</Button>
     <Button type="warning" plain>Warning</Button>
-    <Button type="danger" plain>Danger</Button><br/><br/>
-    <Button size="large">Large</Button>
-    <Button size="small">Small</Button><br/><br/>
-    <Button size="large" loading>Loading</Button>
-    <Button size="large" icon="arrow-up">Icon</Button><br/><br/>    
+    <Button type="danger" plain>Danger</Button><br /><br />
+    <Button round>Round</Button>
+    <Button type="primary" round>Primary</Button>
+    <Button type="success" round>Success</Button>
+    <Button type="info" round>Info</Button>
+    <Button type="warning" round>Warning</Button>
+    <Button type="danger" round>Danger</Button><br /><br />
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.font-primary {
+  font-weight: var(--ya-font-weight-primary);
+  color: var(--ya-text-color-primary);
+}
+</style>
